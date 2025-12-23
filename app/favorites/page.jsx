e2bc9cef2d7d59d0c779a0c74c24c25c4a55ccf8
Nova-techScience <prathameshs157@gmail.com>
2025-12-23 19:59:28 +0530

    updated 23-12-25

--- app/favorites/page.jsx
+++ app/favorites/page.jsx
@@ -1,16 +1,16 @@
 "use client";
 
-import PageWrapper from "@/components/PageWrapper";
+// import PageWrapper from "@/components/PageWrapper";
 
 export default function FavoritesPage() {
   return (
-    <PageWrapper>
+    
       <section className="max-w-3xl">
         <h1 className="text-4xl font-light mb-4">Favorites</h1>
         <p className="text-white/60">
           Your saved companions will appear here.
         </p>
       </section>
-    </PageWrapper>
+    
   );
 }
